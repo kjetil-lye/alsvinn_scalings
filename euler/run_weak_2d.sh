edfb35dfdb6012e39e0dee9c22a0788532ecffc6
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2018-02-10 14:38:00 +0100

    fixed weak run

--- euler/run_weak_2d.sh
+++ euler/run_weak_2d.sh
@@ -21,7 +21,7 @@ function alsvinn_cmake () {
 
     $HOME/local_cmake/bin/cmake ..
 }
-
+cwd=`pwd`
 export OMP_NUM_THREADS=1
 # First we download and compile alsvinn
 git clone --recursive git@github.com:kjetil-lye/alsvinn.git
@@ -31,30 +31,35 @@ cd build
 alsvinn_cmake
 set -e
 make
+cd $cwd
 
+echo $cwd
 BASE_N=256
 cp -r configs/* runs/multiy/
 cp -r configs/* runs/multix/
 cp -r configs/* runs/multixmultiy/
 
-cwd=`pwd`
+
 for N in 256 512 1024 2048 4096 8192;
 do
+    cd $cwd;
     cpus=$((($N*$N)/($BASE_N*$BASE_N)))
     # first we do multiy
     cd runs/multiy/2d/weak_scaling/kelvinhelmholtz_${N}/
-    bsub -n ${cpus} -J weak_chain -W 24:00 mpirun -np ${N} ${cwd}/alsvinn/build/alsuqcli --multi-y ${cpus} kelvinhelmholtz/kelvinhelmholtz.xml
+    bsub -n ${cpus} -J weak_chain -W 24:00 mpirun -np ${cpus} ${cwd}/alsvinn/build/alsuqcli --multi-y ${cpus} kelvinhelmholtz/kelvinhelmholtz.xml
 
     cd $cwd
     cd runs/multix/2d/weak_scaling/kelvinhelmholtz_${N}/
-    bsub -n ${cpus} -J weak_chain -W 24:00 mpirun -np ${N} ${cwd}/alsvinn/build/alsuqcli --multi-x ${cpus} kelvinhelmholtz/kelvinhelmholtz.xml
+    bsub -n ${cpus} -J weak_chain -W 24:00 mpirun -np ${cpus} ${cwd}/alsvinn/build/alsuqcli --multi-x ${cpus} kelvinhelmholtz/kelvinhelmholtz.xml
 
     cd $cwd
     cd runs/multixmultiy/2d/weak_scaling/kelvinhelmholtz_${N}/
     x_cpus=$(($N/${BASE_N}))
-    bsub -n ${cpus} -J weak_chain -W 24:00 mpirun -np ${N} ${cwd}/alsvinn/build/alsuqcli --multi-x ${x_cpus} --multi-y ${x_cpus} kelvinhelmholtz/kelvinhelmholtz.xml
+    bsub -n ${cpus} -J weak_chain -W 24:00 mpirun -np ${cpus} ${cwd}/alsvinn/build/alsuqcli --multi-x ${x_cpus} --multi-y ${x_cpus} kelvinhelmholtz/kelvinhelmholtz.xml
 done;
-bsub -J weak_chain -w "done(weak_chain)"  -N -B $HOME/.local/bin/jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute notebooks/WeakScalingReport.ipynb --output reports/WeakScalingReport_euler_$(date +%Y%m%d).ipynb
+
+cd $cwd;
+cd notebooks; bsub -J weak_chain -w "done(weak_chain)"  -N -B $HOME/.local/bin/jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute WeakScalingReport.ipynb --output ../reports/WeakScalingReport_euler_$(date +%Y%m%d).ipynb
     
     
 
