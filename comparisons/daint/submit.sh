beab73c4c1f4f7344fe74121185070fba2f13a7a
Kjetil Olsen Lye <kjetil.lye@sam.math.ethz.ch>
2018-02-13 23:06:39 +0100

    Cuda reports

--- comparisons/daint/submit.sh
+++ comparisons/daint/submit.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 function alsvinn_cmake {
-    cmake .. -DCMAKE_BUILD_TYPE=Release -DALSVINN_BUILD_TESTS=OFF -DALSVINN_BUILD_DOXYGEN=OFF -DCMAKE_CXX_COMPILER=`which CC` -DCMAKE_PREFIX_PATH=$HOME/pnetcdf/ -DPYTHON_LIBRARY=$(dirname $(dirname $(which python)))/lib/libpython2.7.so -DPYTHON_INCLUDE_DIR=$(dirname $(dirname $(which python)))/include/python2.7
-
+    cmake .. -DCMAKE_BUILD_TYPE=Release -DALSVINN_BUILD_TESTS=OFF -DALSVINN_BUILD_DOXYGEN=OFF -DCMAKE_CXX_COMPILER=`which CC` -DCMAKE_PREFIX_PATH=$HOME/pnetcdf_new/ -DPYTHON_LIBRARY=$(dirname $(dirname $(which python)))/lib/libpython2.7.so -DPYTHON_INCLUDE_DIR=$(dirname $(dirname $(which python)))/include/python2.7
+    safeNETCDF_DIR=${NETCDF_DIR//\//\\\/};
+    sed -i "s/NETCDF_INCLUDE_DIR-NOTFOUND/${safeNETCDF_DIR}\/include/g" CMakeCache.txt
+    sed -i "s/NETCDF_LIBRARY-NOTFOUND/${safeNETCDF_DIR}\/lib\/libnetcdf.so/g" CMakeCache.txt
+    cmake ..
 }
 
 cwd=`pwd`
